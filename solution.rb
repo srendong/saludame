f635abcbdd7e08df7bd80aca94c5f7141fee4e2c
Samuel Rendon G <s.redon.g@gmail.com>
2019-03-19 17:05:04 -0500

    saludame3

--- solution.rb
+++ solution.rb
@@ -5,6 +5,6 @@ get '/' do
   "<h1>Hola desconocido!</h1>" # si ingreso al local host sin un parametro imprime esto
 
   else
-    "<h1>Hola #{params [:nombre]}!</h1>" #si ingreso al localhost con un parametro imprime esto
+    "<h1>Hola #{params[:nombre]}!</h1>" #si ingreso al localhost con un parametro imprime esto
   end
 end
