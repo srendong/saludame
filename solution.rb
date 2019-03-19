fc90fbf05b78c51c90d4a810c95c5dfed872f824
Samuel Rendon G <s.redon.g@gmail.com>
2019-03-19 16:34:14 -0500

    saludame2

---
+++ solution.rb
@@ -0,0 +1,10 @@
+require 'sinatra'
+
+get '/' do
+  unless params[:nombre]
+  "<h1>Hola desconocido!</h1>" # si ingreso al local host sin un parametro imprime esto
+
+  else
+    "<h1>Hola #{params [:nombre]}!</h1>" #si ingreso al localhost con un parametro imprime esto
+  end
+end
